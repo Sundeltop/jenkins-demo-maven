5b0c1665570495b77803af77c7b10d56f4675715
Oleksandr Nelipa <oleksandr_nelipa@epam.com>
2023-06-08 21:42:42 +0300

    Initial commit

---
+++ src/test/java/MySimpleTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class MySimpleTest {
+
+    @Test
+    void demo() {
+        assertTrue(true);
+    }
+}
